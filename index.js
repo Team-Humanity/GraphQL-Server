29b83ed01a0dae5661974727c87b0d456b608f1d
Josh Oppenheim <joshoppenheim@joshs-mbp.lan>
2020-04-07 20:12:29 -0400

    updated to run on GCP cloud run

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const typeDefs = gql`
     email: String!
     about: String
     address: Address
+    profilePicture: ProfilePicture!
   }
 
   type Address {
@@ -18,11 +19,44 @@ const typeDefs = gql`
     stateAbbr: String
   }
 
+  type ProfilePicture {
+    fileName: String!
+  }
+
+  input UserInput {
+    id: String!
+    firstName: String!
+    lastName: String!
+    age: Int
+    about: String!
+    email: String!
+    profilePicture: ProfilePictureInput!
+    address: AddressInput!
+  }
+
+  input ProfilePictureInput {
+    fileName: String!
+  }
+
+  input AddressInput {
+    streetAddress: String!
+    aptSuit: String
+    state: String!
+    zipCode: Int!
+    stateAbbr: String
+  }
+
   type Query {
     user: User
   }
+
+  type Mutation {
+    createProfile(user: UserInput!): User!
+  }
+
   schema {
     query: Query
+    mutation: Mutation
   }
 `;
 
@@ -39,13 +73,16 @@ const resolvers = {
         address: {
           streetAddress: '123 marigold lane',
           state: 'GA',
-          zipCode: 123456
+          zipCode: 123456,
+        },
+        profilePicture: {
+          fileName: "fileSave.txt"
         }
       };
-    }
-  }
+    },
+  },
 };
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({ cors: { origin: '*' }, typeDefs, resolvers });
 server.listen().then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
 });
